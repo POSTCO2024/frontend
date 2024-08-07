04a67f7c6bad81359c619f1b903f07feec7902f3
hanriverkitty <danieltls@naver.com>
2024-08-07 12:52:15 +0900

    karina and pinky's layout fusion #finish

--- apps/operation/src/App.tsx
+++ apps/operation/src/App.tsx
@@ -5,34 +5,100 @@ import { Navigation } from '@postcoil/ui';
 import './App.css';
 import logo from './assets/logo.svg';
 import human from './assets/human.svg';
+// import {
+//   UploadOutlined,
+//   UserOutlined,
+//   VideoCameraOutlined,
+// } from '@ant-design/icons';
+import { Layout, Menu, theme } from 'antd';
 
-function App() {
-  const [count, setCount] = useState(0);
+const { Content, Footer, Sider } = Layout;
 
-  return <Navigation logo={logo} human={human} />;
+// const items = [
+//   UserOutlined,
+//   VideoCameraOutlined,
+//   UploadOutlined,
+//   UserOutlined,
+// ].map((icon, index) => ({
+//   key: String(index + 1),
+//   icon: React.createElement(icon),
+//   label: `nav ${index + 1}`,
+// }));
 
-  // return (
-  //   <>
-  //     <div>
-  //     <Header title='테스트'/>
-  //       <a href="https://react.dev" target="_blank">
-  //         <img src={reactLogo} className="logo react" alt="React logo" />
-  //       </a>
-  //     </div>
-  //     <h1>Vite + React + Operation</h1>
-  //     <div className="card">
-  //       <button onClick={() => setCount((count) => count + 1)}>
-  //         count is {count}
-  //       </button>
-  //       <p>
-  //         Edit <code>src/App.tsx</code> and save to test HMR
-  //       </p>
-  //     </div>
-  //     <p className="read-the-docs">
-  //       Click on the Vite and React logos to learn more
-  //     </p>
-  //   </>
-  // )
-}
+const App: React.FC = () => {
+  // const [count, setCount] = useState(0)
+  const {
+    token: { colorBgContainer, borderRadiusLG },
+  } = theme.useToken();
+
+  return (
+    <div style={{ display: 'flex', flexDirection: 'row', height: '100%' }}>
+      <Navigation logo={logo} human={human} />
+      <Layout>
+        <Header style={{ padding: 0, background: colorBgContainer }} />
+        <Content style={{ margin: '24px 16px 0' }}>
+          <div
+            style={{
+              padding: 24,
+              minHeight: 360,
+              background: colorBgContainer,
+              borderRadius: borderRadiusLG,
+            }}
+          ></div>
+        </Content>
+        <Footer style={{ textAlign: 'center' }}>
+          Ant Design ©{new Date().getFullYear()} Created by Ant UED
+        </Footer>
+      </Layout>
+
+      {/* <div>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p> */}
+    </div>
+  );
+};
+
+// function App() {
+//   const [count, setCount] = useState(0);
+//
+// return <Navigation logo={logo} human={human} />;
+
+// return (
+//   <>
+//     <div>
+//     <Header title='테스트'/>
+//       <a href="https://react.dev" target="_blank">
+//         <img src={reactLogo} className="logo react" alt="React logo" />
+//       </a>
+//     </div>
+//     <h1>Vite + React + Operation</h1>
+//     <div className="card">
+//       <button onClick={() => setCount((count) => count + 1)}>
+//         count is {count}
+//       </button>
+//       <p>
+//         Edit <code>src/App.tsx</code> and save to test HMR
+//       </p>
+//     </div>
+//     <p className="read-the-docs">
+//       Click on the Vite and React logos to learn more
+//     </p>
+//   </>
+// )
+// }
 
 export default App;
--- packages/common-ui/components/header.tsx
+++ packages/common-ui/components/header.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
+import { Breadcrumb } from 'antd';
+import type { BreadcrumbProps } from 'antd';
 
-interface HeaderProps {
-  title: string;
-}
+export const Header: React.FC = () => {
+  const items: BreadcrumbProps['items'] = [
+    {
+      title: 'Home',
+    },
+    {
+      title: <a href="">작업대상재 편성</a>,
+    },
+    {
+      title: <a href="">공정 별 에러재 관리</a>,
+    },
+  ];
 
-export const Header: React.FC<HeaderProps> = ({ title }) => {
-  return (
-    <div>
-      <button>{title}</button>
-    </div>
-  );
+  return <Breadcrumb items={items} />;
 };
+
+export default Header;
