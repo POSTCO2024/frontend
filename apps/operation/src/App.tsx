19cbb10f1577ba4d23559989a50eaf6414378db7
Kim, Yerim <yerim343@naver.com>
2024-08-08 14:37:41 +0900

    scss fix

--- apps/operation/src/App.tsx
+++ apps/operation/src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Routes } from 'react-router-dom';
 import { Navigation } from '@postcoil/ui';
 import './App.css';
 import logo from './assets/logo.svg';
