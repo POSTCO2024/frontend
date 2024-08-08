edca399c4ccc00b904e6edb8660454fa5459becf
Kim, Yerim <yerim343@naver.com>
2024-08-08 19:53:22 +0900

    chore: add log config

--- apps/control/vite.config.ts
+++ apps/control/vite.config.ts
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react-swc';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      onwarn(warning, warn) {
+        console.warn(warning);
+      },
+    },
+  },
+  logLevel: 'error'
 })
